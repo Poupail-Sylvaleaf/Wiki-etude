4595d6f88e7f396c89fac3a25f2f38672c516a5a
mat89 <77246460+mat8950@users.noreply.github.com>
2023-09-27 11:19:58 +0200

    journé2

---
+++ Bachelor/Python/Exo/Exo-base/Exo1.py
@@ -0,0 +1,19 @@
+# exo1
+print('hello')
+# exo 2
+def display_hello():
+    print('hello')
+# exo 3
+try:
+    with open(file='test.txt', mode='r') as f:
+        f.read()
+except FileNotFoundError:
+    print('this file does not exist.')
+
+# exo 4
+intArray = [1, 10, 9, 101]
+for item in intArray:
+    print(item)
+    
+# exo 5
+
--- Bachelor/Python/Exo/Python-exo3.py
+++ Bachelor/Python/Exo/Python-exo3.py
@@ -1,9 +1,24 @@
-L = ['abcabcabcabcabcabcabcabcabc']
-
+# définir une fonction qui prend en paramètre la liste des mms
 def solution(mms):
+    print(f'Taille du gateau : {len(mms)}')
+# partir d'1 part de taille 1
+# si une part est différente, arrêter la comparaison, et augmenter de nouveau la taille de part
     for size_slice in range(1,len(mms)):
-        for other_slice in range(size_slice,len(mms),2):
-            if mms[0:size_slice] != mms[?:?+size_slice]
-    
-    return len(mms) / size_slice
-                                 
+       # print(f'Taille de part : {size_slice}')
+# comparer cette part avec toutes les autres parts
+        slices_are_different = False
+        for start_other_slice in range(size_slice,len(mms),size_slice):
+            #print(f'Index de la prochaine part à comparer : {start_other_slice}')
+            #print(f'Ref : {mms[0:size_slice]}, Current : {mms[start_other_slice:start_other_slice + size_slice]}')
+            if mms[0:size_slice] != mms[start_other_slice : start_other_slice+size_slice]:
+                #arreter la comparaison et augmenter tout de suite la taille de la part            
+                slices_are_different = True
+                break 
+            if not slices_are_different : #verification de l'égalité des parts
+                return int(len(mms) / size_slice)
+        return 1                            
+# si toutes les parts sont égales, alors on a la taille de la part
+#    return len(mms) / size_slice
+
+#solution('abababababab')
+solution('abcabcabcabc')
